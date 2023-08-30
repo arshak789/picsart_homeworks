773fee47216a79a10da01a7f60762907005a5317
arshak <alxaron@gmail.com>
2023-08-31 01:49:52 +0400

    homework1

---
+++ arr_max_int.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	const int size = 5;
+	int arr[5];
+	printf("You must enter five numbers\n");
+	for(int i = 0; i < size; ++i) {
+	printf("Enter number : ");
+	scanf("%d", &arr[i]);
+	}
+	int max = arr[0];
+	for(int i = 1; i < size; ++i) {
+		if(max < arr[i]) {
+			max = arr[i];
+		}	
+	}
+
+	printf("Greatest number of this array : %d\n", max);
+	return 0;
+}
