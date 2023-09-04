abc689a677aab400be385e7ac084ad6f28065368
arshak <alxaron@gmail.com>
2023-09-05 03:25:59 +0400

    homework for august 28

--- little_endian_big_endian.c
+++ little_endian_big_endian.c
@@ -4,19 +4,14 @@
 int main()
 {
 	uint32_t value = 0x12345678;
-
 	printf("Original Value: 0x%08X\n", value);
-
-	//Interpret the value as bytes
 	unsigned char *bytes = (unsigned char *)&value;
-
-	// Check if it's Big Endian or Little Endian
 	if (bytes[0] == 0x12) {
 		printf("The system is Big Endian.\n");
 	} else if (bytes[3] == 0x12) {
 		printf("The system is Little Endian.\n");
 	} else {
-		printf("The system has a different endianness (Middle Endian?).\n");
+		printf("Something went wrong.\n");
 	}
 	return 0;
 }
